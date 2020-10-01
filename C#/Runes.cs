1a012b51ee90779dd465aeaf72ccb88c6124bbd1
cdbuchanan <43014122+cdbuchanan@users.noreply.github.com>
2020-10-01 00:50:23 -0500

    Update Runes.cs
    I fixed the bug that was removing the negative from the second number (p2). All the tests now pass.

--- C#/Runes.cs
+++ C#/Runes.cs
@@ -30,11 +30,11 @@ public class Runes
     
     // p1 +-* p2 = ans
     string p1 = pts[0][0].ToString(); //adds the first digit to the p1
-    string op = "";
-    string p2 = "";
+    string op = ""; //operator
+    string p2 = ""; //second part
     string ans = pts[1];
     
-    //end with subtraction last, due to negative numbers. 
+    //discovering operator. end with subtraction last, due to negative numbers. 
     if (pts[0].Contains("+")) {op = "+";}
     else if (pts[0].Contains("*")) {op = "*";}
     else 
@@ -44,63 +44,42 @@ public class Runes
       for (int i = 1; i < pts[0].Length; i++)
       { //assign p1 & p2
         string c = pts[0][i].ToString();
-        if (c == "-") {p2Reached = true;}
+        if (c == "-" && !p2Reached) {p2Reached = true;}
         else if (!p2Reached) {p1 += c;}
         else {p2 += c;}
       }
-    } 
+    }
+    
     if (op == "+" || op == "*") 
     { // assign p1 & p2
       string[] tmp = pts[0].Split(op);
       p1 = tmp[0];
       p2 = tmp[1];
     }
+    
     List<string> unavailable = new List<string>();
     foreach (char c in expression) {unavailable.Add(c.ToString());}
     
-    int current = 0;
-    //string current = p1 + "." + op + "." + p2 + "=" + ans + "\n";
-    
-    try 
-    {
-      for (int i = 0; i < 10; i++)
-      {
-        //current = i;
-      if (!unavailable.Contains(i.ToString())) 
-      {
-        /*if (i == 0 && (p1[0] == '?' || p2[0] == '?' || ans[0] == '?')) 
-            {
-          //do nothing
-        } 
-        else 
+    for (int i = 0; i < 10; i++)
+      {        
+        if (!unavailable.Contains(i.ToString())) 
         {
+          int tryP1 = Int32.Parse(p1.Replace("?", i.ToString()));
           
-        }*/
-        int t1 = Int32.Parse(p1.Replace("?", i.ToString()));
-        
-        int t2 = Int32.Parse(p2.Replace("?", i.ToString()));
-        int tAns = Int32.Parse(ans.Replace("?", i.ToString()));
-        if (t1.ToString().Length == p1.Length && p2.ToString().Length == p2.Length && tAns.ToString().Length == ans.Length)
-        {
-          int testAnswer = 0;
-          if (op == "+") {testAnswer = t1 + t2;}
-          else if (op == "*") {testAnswer = t1 * t2;}
-          else testAnswer = t1 - t2;
-          if (testAnswer == tAns) {return i;}  
+          int tryP2 = Int32.Parse(p2.Replace("?", i.ToString()));
+          int tryAns = Int32.Parse(ans.Replace("?", i.ToString()));
+          if (tryP1.ToString().Length == p1.Length && tryP2.ToString().Length == p2.Length && tryAns.ToString().Length == ans.Length)
+          {
+            int testAnswer = 0;
+            if (op == "+") {testAnswer = tryP1 + tryP2;}
+            else if (op == "*") {testAnswer = tryP1 * tryP2;}
+            else testAnswer = tryP1 - tryP2;
+            if (testAnswer == tryAns) {return i;}  
+          }
+          
         }
-        
-        
-        
-        
-      }
-    }
-    }
-    catch
-    {
-      return current;
-    }
-              
-    return -1; //nothing found
+      }     
+      return -1; //nothing found
+    
   }
 }
-
